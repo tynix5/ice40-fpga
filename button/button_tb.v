ffba66d80b969e64d7195791e85c72a1c432d015
tyler <tynix5@gmail.com>
2024-07-24 21:45:38 -0500

    Improved tb

--- button/button_tb.v
+++ button/button_tb.v
@@ -1,28 +1,36 @@
+`timescale 1ns/1ps
+
 module button_tb();
 
-wire SWITCH;
-wire LED;
+reg switch;
+wire out;
 
-button test(.switch(SWITCH), .led(LED));
+button test(.switch(switch), .led(out));
 
 
 initial
  begin
     $dumpfile("button_tb.vcd");
     $dumpvars(0,button_tb);
+    #50
+    $display("Done.");
+    $finish;
  end
 
 
-initial begin
+always begin
 
-    $display("Switch State: %b ... LED State: %b", SWITCH, LED);
-
+    #5
+    $display("tick %d: Switch State: %b ... LED State: %b", $time, switch, out);
 end
 
 always begin
 
-    LED = SWITCH;
+    switch = 0;
     #10
+    switch = 1;
+    #10
+    switch = 0;
 end
 
 
